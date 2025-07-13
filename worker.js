b882b31df502dccf7f6343123e3374686ef061a6
workers7 <solar.farm.tallbear@gmail.com>
2025-07-13 13:32:39 -0700

    Update worker.js
    added yserID

--- worker.js
+++ worker.js
@@ -198,7 +198,7 @@ var cf_worker_vless_default = {
     async fetch(request, env, ctx) {
         let address = "";
         let portWithRandomLog = "";
-        const userID = env.UUID || "00fdxxxd-81xx-xx54-bxx2-b6xxx2aXXXXX"; // ENTER YOUR OWN UUID HERE
+        const userID = env.UUID || "80d1fe59-60f4-4272-a280-820e1ef0a754"; // ENTER YOUR OWN UUID HERE
         const isVaildUUID = validate_default(userID);
         const log = (info, event) => {
             console.log(`[${address}:${portWithRandomLog}] ${info}`, event || "");
